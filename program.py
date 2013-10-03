f19682251054e0a9d61ca6614947d829fa951fe5
mprat <michele.pratusevich@gmail.com>
2013-10-03 14:40:44 +0300

    number-checking moved into askUserForNumber method

--- program.py
+++ program.py
@@ -1,3 +1,4 @@
+# returns a list of divisors of n
 def divisors(n):
     list_of_divisors = []
     if n >= 2:
@@ -7,22 +8,26 @@ def divisors(n):
     list_of_divisors.append(n)
     return list_of_divisors
 
+# function to ask a user for a number
 def askUserForNumber():
-     return raw_input("Type the number you want to see the divisors of: ")
+    num = raw_input("Type a number: ")
+    #check if user entered a number
+    while not (num.isdigit()):
+        num = raw_input("That is not a number. \nType a number: ")
+    return num
 
+# return True/False if the number is prime
 def isPrime(n):
     if len(divisors(n)) == 2:
         return True
     return False
 
 if __name__ == '__main__':
-    # get input from the user
+    #make a continuous loop
     while (True):
         num = askUserForNumber()
 
-        while not (num.isdigit()):
-            num = askUserForNumber()
-
         print divisors(int(num))
+        #print sentence only if number is prime
         if (isPrime(int(num))):
             print "The number is prime!"
